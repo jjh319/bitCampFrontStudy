8b76de99521f47326c1f62a8547157ea5ce193ab
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-01-19 10:47:06 +0900

    join 학습 및 문제풀기

---
+++ sql/사용자_JAVA.sql
@@ -0,0 +1,50 @@
+-- 문제 1 GROUP BY/HAVING)
+SELECT
+    major,
+    trunc(avg(avr),1) AS 평균
+FROM
+    student
+GROUP BY
+    major
+HAVING
+    major in('생물','물리','유공','식영');
+    
+SELECT
+    major,
+    trunc(avg(avr),1) AS 평균
+FROM
+    student
+GROUP BY
+    major
+HAVING
+    major in('생물','물리','유공','식영') AND
+    trunc(avg(avr),1) < 2;
+    
+    
+-- 문제1 JOIN)
+SELECT
+    pno,
+    pname,
+    cno,
+    cname
+FROM
+    professor
+    JOIN course  using(pno);
+  
+SELECT
+    p.pno,
+    p.pname,
+    c.cno,
+    c.cname
+FROM
+    professor p
+    JOIN course c ON p.pno = c.pno
+WHERE
+    c.cname LIKE '%화학';
+
+
+
+
+
+
+
