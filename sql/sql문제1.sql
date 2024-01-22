5030883b8d450b6a2ee122546b19869d3de233ac
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-01-22 11:16:00 +0900

    sql union 학습 및 문제풀기

--- sql/sql문제1.sql
+++ sql/sql문제1.sql
@@ -283,6 +283,53 @@ FROM employees e
 JOIN employees m on(m.employee_id = e.manager_id);
 
 
+create table employees_role as select * from employees where 1=0;
+
+insert into employees_role values(101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', 
+'1989-09-21', 'AD_VP', 17000.00, NULL, 100, 90);
+insert into employees_role values(101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', 
+'1989-09-21', 'AD_VP', 17000.00, NULL, 100, 90);
+insert into employees_role values(101, 'Nee', 'Ko', 'NKOCHHAR', '515.123.4568', 
+'1989-09-21', 'AD_VP', 17000.00, NULL, 100, 90);
+insert into employees_role values(200, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', 
+'1989-09-21', 'AD_VP', 17000.00, NULL, 100, 90);
+insert into employees_role values(200, 'Nee', 'Kochhar', 'NKOCHHAR', '515.123.4568', 
+'1989-09-21', 'AD_VP', 17000.00, NULL, 100, 90);
+insert into employees_role values(300, 'GilDong', 'Hong', 'NKOCHHAR', '010-123-4567', 
+'2009-03-01', 'IT_PROG', 23000.00, NULL, 100, 90);
+commit;
+
+SELECT employee_id, last_name FROM employees
+union
+SELECT employee_id, last_name FROM employees_role;
+
+SELECT employee_id, last_name FROM employees
+intersect
+SELECT employee_id, last_name FROM employees_role;
+
+-- 문제1)
+SELECT 
+    last_name AS 사원이름,
+    job_id AS 업무ID,
+    department_id AS 부서ID
+FROM
+    employees
+WHERE
+    department_id in('10')
+UNION
+SELECT
+    last_name,
+    job_id,
+    department_id
+FROM
+    employees_role
+WHERE
+    job_id in('IT_PROG');
+
+
+
+
+
 
 
 
