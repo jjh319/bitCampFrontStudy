1efbf4c4535edc8deba82bb7c1bfcbf7811147bc
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-01-26 12:23:45 +0900

    sql 문제풀이

--- sql/사용자_JAVA.sql
+++ sql/사용자_JAVA.sql
@@ -81,9 +81,46 @@ GROUP BY
 HAVING
     avg(RESULT) >= 60;
     
+-- 기말고사 평균 성적이 핵 화학과목보다 우수한 과목의 과목명과 담당 교수명을 검색하시오
+SELECT
+    CNO,
+    CNAME,
+    avg(RESULT),
+    PNO,
+    PNAME
+FROM
+    student JOIN score using(sno) JOIN course using(CNO) JOIN professor using(PNO)
+GROUP BY
+    CNO,CNAME,PNO,PNAME
+HAVING
+    avg(RESULT) > (SELECT avg(result)
+                   FROM score JOIN course using(CNO) JOIN professor using(pno)
+                   
 
 
-    
+ 
+SELECT A.CNO
+       , A.CNAME
+       , A.RESULT
+       , B.PNO
+       , B.PNAME
+FROM (
+		SELECT C.CNO
+			  , C.CNAME
+			  , C.PNO
+			  , ROUND(AVG(D.RESULT), 2) AS RESULT
+		FROM COURSE C, SCORE D
+		WHERE C.CNO = D.CNO
+		GROUP BY C.CNO, C.CNAME, C.PNO
+       ) A
+       , PROFESSOR B
+WHERE A.PNO = B.PNO AND A.RESULT > (
+                                SELECT ROUND(AVG(D.RESULT), 2)
+                                FROM COURSE C, SCORE D
+                                WHERE C.CNO = D.CNO
+                                GROUP BY C.CNO, C.CNAME
+                                HAVING C.CNAME = '핵화학'
+                               );   
 
 
 
